617d592e8df6acb9d2c3c9505ccb5ed99c5df460
kabirules <javifont@gmail.com>
2018-08-28 00:04:08 +0200

    Chapter 6

---
+++ Assets/_Scenes/World/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	[SerializeField] private Text xpText;
+	[SerializeField] private Text levelText;
+	[SerializeField] private GameObject menu;
+
+	private void Awake()
+	{
+		Assert.IsNotNull(xpText);
+		Assert.IsNotNull(levelText);
+		Assert.IsNotNull(menu);
+	}
+
+	public void UpdateLevel(int level)
+	{
+		levelText.text = level.ToString();
+	}
+
+	public void UpdateXP(int currentXP, int requiredXP)
+	{
+		xpText.text = currentXP.ToString() + " / " + requiredXP.ToString();
+	}
+
+	public void ToggleMenu() 
+	{
+		menu.SetActive(!menu.activeSelf);
+	}
+}
